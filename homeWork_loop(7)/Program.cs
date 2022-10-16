f12dbf1aa06159d0d923f2ebe0e51696548a9d71
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-16 14:07:43 +0500

    Добавьте файлы проекта.

---
+++ homeWork_loop(7)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_loop_7_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startCycle = 5;
+            int endCycle = 96;
+            int stepCycle = 7;
+            for (int i = startCycle; i <= endCycle; i += stepCycle) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
