1ca194a9b69e41358a037eeca5e7dfe40cf86687
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-17 18:32:47 +0500

    1)Добавлены пустые строки 2)Изменен вывод

--- homeWork_loop(7)/Program.cs
+++ homeWork_loop(7)/Program.cs
@@ -13,10 +13,12 @@ namespace homeWork_loop_7_
             int startCycle = 5;
             int endCycle = 96;
             int stepCycle = 7;
+
             for (int i = startCycle; i <= endCycle; i += stepCycle) 
             {
-                Console.WriteLine(i);
+                Console.Write($"{i} ");
             }
+
             Console.ReadKey();
         }
     }
